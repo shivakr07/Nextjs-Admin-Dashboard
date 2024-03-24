14353919ecf23bf8e14b701a18c180b7a2ec4454
shivakr07 <kumarshiva0707@gmail.com>
2024-03-24 16:35:44 +0530

    updated user page with key prop

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -32,37 +32,35 @@ const User = async() => {
           </thead>
 
           <tbody>
-            {
-              users.map(user => (
-                <tr>
-                  <td>
-                    <div className={styles.user}>
-                      <Image src="/avatar.svg"
-                        width={40}
-                        height={40}
-                        className={styles.userImage}
-                      />
-                      {user.username}
-                    </div>
-                  </td>
-                  <td>{user.email}
-                  </td>
-                  <td>{user.createdAt ?.toString().slice(4, 16)}</td>
-                  <td>{user.isAdmin ? "Admin" : "Not Admin"}</td>
-                  <td>{user.isActive ? "Online" : "Offline"}</td>
-                  <td>
-                    <Link href={`/dashboard/users/${user.id}`}>
-                      <button className={`${styles.button} ${styles.view}`}>View</button>
-                    </Link>
-                    <form action={deleteUser}>
-                      <input type='hidden' name='id' value={user.id} />
-                      <button className={`${styles.button} ${styles.delete}`}>Delete</button>
-                    </form>
-                  </td>
-                </tr>
-          ))
-        }
-          </tbody>
+  {users.map(user => (
+    <tr key={user.id}>
+      <td>
+        <div className={styles.user}>
+          <Image src="/avatar.svg"
+            width={40}
+            height={40}
+            className={styles.userImage}
+          />
+          {user.username}
+        </div>
+      </td>
+      <td>{user.email}</td>
+      <td>{user.createdAt ?.toString().slice(4, 16)}</td>
+      <td>{user.isAdmin ? "Admin" : "Not Admin"}</td>
+      <td>{user.isActive ? "Online" : "Offline"}</td>
+      <td>
+        <Link href={`/dashboard/users/${user.id}`}>
+          <button className={`${styles.button} ${styles.view}`}>View</button>
+        </Link>
+        <form action={deleteUser}>
+          <input type='hidden' name='id' value={user.id} />
+          <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+        </form>
+      </td>
+    </tr>
+  ))}
+</tbody>
+
         </table>
     </div>
   )
